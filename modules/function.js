8e2a6dcbb62d7052bb6dfaa880d829abc9a7dc57
BatohaShir <batosirapov94@gmail.com>
2024-05-04 22:28:30 +0800

    fixed funcion file

--- modules/function.js
+++ modules/function.js
@@ -1,16 +1,22 @@
 export function LoginInSystem(users) {
-    while (true){
+    while (true) {
+        let user_names = Object.keys(users);
         let login = prompt('Введите ваш логин:');
         let password = prompt('Введите ваш пароль:');
-        if (users.hasOwnProperty(login) && users[login] === password){
-            console.log('╔═════════════════════════════════╗');
-            console.log('║        Добро пожаловать в       ║');
-            console.log('║            систему!             ║');
-            console.log('╚═════════════════════════════════╝');
-            console.log(`==========ПРИВЕТ! ${login}==========`)
-            break;
-        }else{
-            console.log('НЕВЕРНЫЙ ЛОГИН ИЛИ ПАРОЛЬ!!!')
+        
+        if (user_names.includes(login)) {
+            if (users[login] === password) {
+                console.log('╔═════════════════════════════════╗');
+                console.log('║        Добро пожаловать в       ║');
+                console.log('║            систему!             ║');
+                console.log('╚═════════════════════════════════╝');
+                console.log(`==========ПРИВЕТ! ${login}==========`)
+                break;
+            } else {
+                console.log('INCORRECT PASSWORD!!!');
+            }
+        } else {
+            console.log('USER NOT FOUND!!!');
         }
     }
 }
